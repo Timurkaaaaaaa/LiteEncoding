d8f0cf08aefc1b05acbc5abc52d20e4965a9e3eb
Timurkaaa <timurzarovskiy@gmail.com>
2025-02-08 16:14:05 +0300

    🟢 Create main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ class MainApp(QtWidgets.QMainWindow, Ui_LiteEncoding):
         super().__init__()
         self.setupUi(self)
 
-        # Подключение кнопок к методам
         self.EncodeASCII16.clicked.connect(self.encode_ascii16)
         self.EncodeASCII10.clicked.connect(self.encode_ascii10)
         self.EncodeASCII2.clicked.connect(self.encode_ascii2)
@@ -18,26 +17,45 @@ class MainApp(QtWidgets.QMainWindow, Ui_LiteEncoding):
         self.DecodeBASE64.clicked.connect(self.decode_base64)
 
     def encode_ascii16(self):
-        input_text = self.InputText.toPlainText()  # Получаем текст из многострочного поля
+        input_text = self.InputText.toPlainText()
         self.OutputText.setText(input_text.encode("windows-1251", "replace").hex())
 
     def encode_ascii10(self):
-        print("Нажата кнопка Encode ASCII (decimal)")
+        input_text = self.InputText.toPlainText()
+        encoded_bytes = input_text.encode('windows-1251')
+        output_string = ''.join(str(byte) for byte in encoded_bytes)
+        self.OutputText.setText(output_string)
 
     def encode_ascii2(self):
-        print("Нажата кнопка Encode ASCII (binary)")
+        input_text = self.InputText.toPlainText()
+        encoded_bytes = input_text.encode('windows-1251')
+        binary_string = ''.join(format(byte, '08b') for byte in encoded_bytes)
+        self.OutputText.setText(binary_string)
 
     def encode_base64(self):
         print("Нажата кнопка Encode BASE64")
 
     def decode_ascii16(self):
-        print("Нажата кнопка Decode ASCII (hexadecimal)")
+        # Получаем шестнадцатеричный текст из поля ввода
+        hex_input = self.InputText.toPlainText()
+        bytes_object = bytes.fromhex(hex_input)
+        decoded_string = bytes_object.decode('windows-1251')
+        self.OutputText.setText(decoded_string)
+
 
     def decode_ascii10(self):
-        print("Нажата кнопка Decode ASCII (decimal)")
+        input = self.InputText.toPlainText()
+        byte_values = [int(input[i:i + 3]) for i in range(0, len(input), 3)]
+        decoded_string = bytes(byte_values).decode('windows-1251')
+        self.OutputText.setText(decoded_string)
 
     def decode_ascii2(self):
-        print("Нажата кнопка Decode ASCII (binary)")
+        binary_string = self.InputText.toPlainText()
+        bytes_list = [binary_string[i:i+8] for i in range(0, len(binary_string), 8)]
+
+        # Преобразуем каждый байт из двоичного представления в десятичное и затем в символ
+        decoded_string = ''.join(chr(int(byte, 2)) for byte in bytes_list)
+        self.OutputText.setText(decoded_string)
 
     def decode_base64(self):
         print("Нажата кнопка Decode BASE64")
